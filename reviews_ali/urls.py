ad7c5e8650c18cf15e680e3498bc9abe3ebcb2f9
lautarianoo <neonchick1>
2021-04-12 12:59:47 +0500

    поиск + детализация новостей

--- reviews_ali/urls.py
+++ reviews_ali/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Index
+from .views import Index, NewsDetailView, SearchFilm
 
 urlpatterns = [
-    path('', Index.as_view(), name='index')
+    path('', Index.as_view(), name='index'),
+    path('news/<slug:slug>', NewsDetailView.as_view(), name='news_detail'),
+    path('searchnews/', SearchFilm.as_view(), name='search_news')
 ]
--- reviews_ali/views.py
+++ reviews_ali/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import News
 
 class Index(ListView):
@@ -8,3 +8,18 @@ class Index(ListView):
     queryset = News.objects.all()
     template_name = 'reviews_ali/index.html'
 
+class NewsDetailView(DetailView):
+    model = News
+    slug_field = 'url'
+    template_name = 'reviews_ali/news_detail.html'
+
+class SearchFilm(ListView):
+    template_name = 'reviews_ali/index.html'
+
+    def get_queryset(self):
+        return News.objects.filter(title__icontains=self.request.GET.get('search_news'))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['search_news'] = f'searchnews={self.request.GET.get("search_news")}&'
+        return context
